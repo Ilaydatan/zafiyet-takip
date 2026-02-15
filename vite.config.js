26488ff19489afadc5b1041b406d999fbe35f8e7
Ilaydatan <ilaydanurtan123@gmail.com>
2026-02-15 22:34:16 +0300

    Vite base ayari guncellendi

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Dosya yollarının başına './' ekleyerek Netlify'ın dosyaları bulmasını sağlar.
 })
